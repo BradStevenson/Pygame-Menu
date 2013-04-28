164d0dd16768e357bf664a71e4b4ec624238a7fb
Brad Stevenson <BradStevenson@outlook.com>
2013-04-28 15:36:19 +0100

    Added title bar option

--- Menu.py
+++ Menu.py
@@ -12,17 +12,31 @@ class Menu:
     field = []
     dest_surface = pygame.Surface
     number_fields = 0
+<<<<<<< HEAD
+    background_color = (52, 73, 94)
+    title_font = "pixelated_bold.ttf"
+    title_size = 36
+    font = "pixelated_font.ttf"
+    font_size = 20
+    font_color =  (255, 255, 255)
+    selection_color = (26, 188, 156)
+=======
     background_color = (51, 51, 51)
     font_size = 36
     font_color =  (255, 255, 255)
     selection_color = (153, 102, 255)
+>>>>>>> da37cbac2cd850fbfdfcdcf728d8301564037a0a
     selection_position = 0
     position = (0,0)
     menu_width = 0
     menu_height = 0
 
     class Field:
+<<<<<<< HEAD
+    	text = ''
+=======
       text = ''
+>>>>>>> da37cbac2cd850fbfdfcdcf728d8301564037a0a
     	field = pygame.Surface
     	field_rect = pygame.Rect
     	selection_rect = pygame.Rect
@@ -35,6 +49,20 @@ class Menu:
     	self.font_color = text
     	self.selection_color = selection
 
+<<<<<<< HEAD
+    def set_font(self, title, menu):
+        self.title_font = title
+        self.font = menu
+
+    def get_position(self):
+    	return self.selection_position
+
+    def init(self, item_list, dest_surface, title):
+    	self.field_list = item_list
+    	self.dest_surface = dest_surface
+    	self.number_fields = len(self.field_list)
+        self.title = title
+=======
     def get_position(self):
     	return self.selection_position
 
@@ -42,6 +70,7 @@ class Menu:
     	self.field_list = item_list
     	self.dest_surface = dest_surface
     	self.number_fields = len(self.field_list)
+>>>>>>> da37cbac2cd850fbfdfcdcf728d8301564037a0a
     	self.create_structures()
 
     def draw(self, move=0):
@@ -63,11 +92,26 @@ class Menu:
     def create_structures(self):
         shift = 0
         self.menu_height = 0
+<<<<<<< HEAD
+        self.font = pygame.font.Font(self.font, self.font_size)
+        for i in xrange(self.number_fields):
+            self.field.append(self.Field())
+            self.field[i].text = self.field_list[i]
+            if i == 0:
+                if self.title:
+                    self.title_font = pygame.font.Font(self.title_font, self.title_size)
+                    self.field[i].field = self.title_font.render(self.field[i].text, 1, self.font_color)
+                else:
+                    self.field[i].field = self.font.render(self.field[i].text, 1, self.font_color)
+            else:
+                self.field[i].field = self.font.render(self.field[i].text, 1, self.font_color)
+=======
         self.font = pygame.font.Font(None, self.font_size)
         for i in xrange(self.number_fields):
             self.field.append(self.Field())
             self.field[i].text = self.field_list[i]
             self.field[i].field = self.font.render(self.field[i].text, 1, self.font_color)
+>>>>>>> da37cbac2cd850fbfdfcdcf728d8301564037a0a
 
             self.field[i].field_rect = self.field[i].field.get_rect()
             shift = int(self.font_size * 0.2)
@@ -84,8 +128,15 @@ class Menu:
             	self.menu_width = width
             else:
             	width = self.menu_width
+<<<<<<< HEAD
+
             self.field[i].selection_rect = (left, top , width, height)
             self.menu_height += height
+            
+=======
+            self.field[i].selection_rect = (left, top , width, height)
+            self.menu_height += height
+>>>>>>> da37cbac2cd850fbfdfcdcf728d8301564037a0a
         x = self.dest_surface.get_rect().centerx - self.menu_width / 2
         y = self.dest_surface.get_rect().centery - self.menu_height / 2
         mx, my = self.position
@@ -102,9 +153,18 @@ def main():
 	screen.blit(background,(0, 0))
 
 	menu = Menu()
+<<<<<<< HEAD
+    # Initiate Menu with ([menu items], Surface, TitleBool) True for menu with title, False for without.
+	menu.init(['Start', 'Pause', 'Quit'], screen, False)
+	# Optional to set menu colors. (Text color, Selection color, Menu-Background color)
+	# menu.set_colors((255, 255, 255), (26, 188, 156), (52, 73, 94))
+    # Optional to set menu fonts (Title font, Menu font)
+    # menu.set_fonts("pixelated_bold.ttf", "pixelated_font.ttf")
+=======
 	menu.init(['Start', 'Pause', 'Quit'], screen)
 	# Optional to set menu colors. (Text color, Selection color, Menu-Background color)
 	menu.set_colors((255, 255, 255), (26, 188, 156), (52, 73, 94))
+>>>>>>> da37cbac2cd850fbfdfcdcf728d8301564037a0a
 	menu.draw()
 
 	running = True
@@ -124,4 +184,8 @@ def main():
 				   		running = False
 
 if __name__ == "__main__":
+<<<<<<< HEAD
 	main()
+=======
+	main()
+>>>>>>> da37cbac2cd850fbfdfcdcf728d8301564037a0a
